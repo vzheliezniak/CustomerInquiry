1cd90e697fd0cf8ad6ad0df064a47d2594ce7cc8
Viktoriia Zheliezniak <vzheliezniak@oncologyanalytics.com>
2019-10-07 13:58:46 +0300

    added unit tests

---
+++ BusinessTests/CustomerInquiryManagerTests.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Linq;
+using Business.Implementation;
+using Business.Model;
+using DataAccess.Abstract;
+using DataAccess.Models;
+using Moq;
+using NUnit.Framework;
+
+namespace BusinessTests
+{
+    public class CustomerInquiryManagerTests
+    {
+        private Mock<CustomerInquiryManager> _sut;
+        [SetUp]
+        public void Setup()
+        {
+            var repository = new Mock<ICustomerRepository>();
+            repository.Setup(x => x.GetCustomerWithTransactionsByFilter(It.IsAny<Func<Customer, bool>>()))
+                .Returns((Func<Customer, bool> predicate) => MockData.GetCustomers().Where(predicate).FirstOrDefault());
+            _sut = new Mock<CustomerInquiryManager>(repository.Object);
+        }
+
+        [Test]
+        public void GetCustomerProfile_CustomerIsNotFound_CustomerNotFoundExceptionIsThrown()
+        {
+            Assert.Throws<CustomerNotFoundException>(() => _sut.Object.GetCustomerProfile(25));
+        }
+
+        [TestCase(395, null)]
+        [TestCase(0, "p@gmail.com")]
+        public void GetCustomerProfile_CustomerIsFound_CustomerProfileWithNoTransactionsIsReturned(decimal customerId, string email)
+        {
+            var res = _sut.Object.GetCustomerProfile(customerId, email);
+
+            Assert.NotNull(res);
+            Assert.IsEmpty(res.RecentTransactions);
+        }
+
+        [Test]
+        public void GetCustomerProfile_CustomerIsFound_CustomerIsFound_CustomerProfileWithListOfRecentTransactionsIsReturned()
+        {
+            const decimal customerId = 61995;
+            const int expectedAmountOfTransactions = 5;
+
+            var res = _sut.Object.GetCustomerProfile(customerId);
+
+            Assert.IsNotEmpty(res.RecentTransactions);
+            Assert.AreEqual(expectedAmountOfTransactions, res.RecentTransactions.Count());
+        }
+    }
+}
---
+++ BusinessTests/MockData.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataAccess.Models;
+
+namespace BusinessTests
+{
+    public static class MockData
+    {
+        public static List<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer
+                {
+                    CustomerId = 395,
+                    Name = "Petro Ivanenko",
+                    Email = "p@gmail.com",
+                    MobilePhone = 2849583535,
+                    Transaction = new List<Transaction>()
+                },
+                new Customer
+                {
+                    CustomerId = 31995,
+                    Name = "Semen Ivanenko",
+                    Email = "semen@gmail.com",
+                    MobilePhone = 2800583535,
+                    Transaction = new List<Transaction>
+                    {
+                        new Transaction
+                        {
+                            TransactionId = 1,
+                            Amount = 56,
+                            CurrencyCode = "UAH",
+                            CustomerId = 31995,
+                            Date = new DateTime(2018, 12, 1),
+                            Status = "Success"
+                        },
+                        new Transaction{
+                            TransactionId = 5,
+                            Amount = 51,
+                            CurrencyCode = "UAH",
+                            CustomerId = 31995,
+                            Date = new DateTime(2018, 12, 2),
+                            Status = "Success"
+                        }
+                    }
+                },
+                new Customer
+                {
+                    CustomerId = 61995,
+                    Name = "Adam Peters",
+                    Email = "peters@gmail.com",
+                    MobilePhone = 2800583535,
+                    Transaction = new List<Transaction>
+                    {
+                        new Transaction
+                        {
+                            TransactionId = 7,
+                            Amount = 56,
+                            CurrencyCode = "UAH",
+                            CustomerId = 61995,
+                            Date = new DateTime(2018, 12, 1),
+                            Status = "Success"
+                        },
+                        new Transaction{
+                            TransactionId = 9,
+                            Amount = 51,
+                            CurrencyCode = "UAH",
+                            CustomerId = 61995,
+                            Date = new DateTime(2018, 12, 15),
+                            Status = "Success"
+                        },
+                        new Transaction{
+                            TransactionId = 11,
+                            Amount = 516,
+                            CurrencyCode = "UAH",
+                            CustomerId = 61995,
+                            Date = new DateTime(2019, 4, 7),
+                            Status = "Failure"
+                        },
+                        new Transaction{
+                            TransactionId = 13,
+                            Amount = 51,
+                            CurrencyCode = "UAH",
+                            CustomerId = 61995,
+                            Date = new DateTime(2019, 5, 3),
+                            Status = "Canceled"
+                        },
+                        new Transaction{
+                            TransactionId = 15,
+                            Amount = 51,
+                            CurrencyCode = "UAH",
+                            CustomerId = 61995,
+                            Date = new DateTime(2019, 6, 1),
+                            Status = "Success"
+                        },
+                        new Transaction{
+                            TransactionId = 17,
+                            Amount = 51,
+                            CurrencyCode = "UAH",
+                            CustomerId = 61995,
+                            Date = new DateTime(2019, 6, 10),
+                            Status = "Success"
+                        },
+                        new Transaction{
+                            TransactionId = 19,
+                            Amount = 51,
+                            CurrencyCode = "UAH",
+                            CustomerId = 61995,
+                            Date = new DateTime(2019, 8, 15),
+                            Status = "Success"
+                        },
+                        new Transaction{
+                            TransactionId = 21,
+                            Amount = 51,
+                            CurrencyCode = "UAH",
+                            CustomerId = 61995,
+                            Date = new DateTime(2019, 9, 1),
+                            Status = "Success"
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
